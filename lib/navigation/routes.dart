78833de7098d3d74ebf885c3646b922ad85e4349
nieich <w.nico99.nw@gmail.com>
2025-08-20 18:43:03 +0200

    first routs basic

--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -1,8 +1,17 @@
 class Routes {
   Routes._();
   static const String root = '/';
+
   static const String home = 'Home';
   static const String pathHome = '$root$home';
+  static const String homeQRCode = 'QRCode';
+  static const String pathHomeQRCode = '$pathHome/$homeQRCode';
+
   static const String settings = 'Settings';
   static const String pathSettings = '$pathHome/$settings';
+
+  static const String pass = 'Pass';
+  static const String pathPass = '$pathHome/$pass';
+  static const String passQrCode = 'PassQrCode';
+  static const String pathPassQrCode = '$pathPass/$passQrCode';
 }
